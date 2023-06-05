4df3aeb444870d2bb83cca04c58aea3c0f807b77
Mathias Mollatt <mathias.mollatt@nmbu.no>
2023-06-05 13:46:15 +0200

    Animals uten oppdatering på fitness

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,34 +1,38 @@
 import math
 import random
 
-const = {}
-const["Herbivore"] = {'w_birth': 8.0,
-                      'sigma_birth': 1.5,
-                      'beta': 0.9,
-                      'eta': 0.05,
-                      'a_half': 40.0,
-                      'phi_age': 0.6,
-                      'w_half': 10.0,
-                      'phi_weight': 0.1,
-                      'mu': 0.25,
-                      'gamma': 0.2,
-                      'zeta': 3.5,
-                      'xi': 1.2,
-                      'omega': 0.4,
-                      'F': 10.0,
-                      'DeltaPhiMax': None}
 
-const["Carnivore"] = {}
-const["Landskap"] = {"Lowland":2, "Highland":300}
+
 
 class Animal:
+    const = {}
+    const["Herbivore"] = {'w_birth': 8.0,
+                          'sigma_birth': 1.5,
+                          'beta': 0.9,
+                          'eta': 0.05,
+                          'a_half': 40.0,
+                          'phi_age': 0.6,
+                          'w_half': 10.0,
+                          'phi_weight': 0.1,
+                          'mu': 0.25,
+                          'gamma': 0.2,
+                          'zeta': 3.5,
+                          'xi': 1.2,
+                          'omega': 0.4,
+                          'F': 10.0,
+                          'DeltaPhiMax': None}
 
-    def __init__(self, row, loc):
+    const["Carnivore"] = {}
+    const["Landskap"] = {"Lowland": 800, "Highland": 300}
+
+
+    def __init__(self, row, loc,seed=0):
+        random.seed(seed)
         self.loc = loc
         self.species = row["species"]
         self.age = row["age"]
         self.weight = row["weight"]
-        self.fitness = self.calc_fitness(row, const["Herbivore"]["w_birth"])
+        self.fitness = self.calc_fitness(row, self.const[self.species]["w_birth"])
         self.alive = True
 
 
@@ -37,42 +41,46 @@ class Animal:
         if w_birth < 0:
             fitness = 0
         else:
-            fitness = 1/(1+math.exp(const["Herbivore"]["phi_age"]*(row["age"]-const["Herbivore"]["a_half"]))) * 1/(1+math.exp(-const["Herbivore"]["phi_weight"]*(row["weight"]-const["Herbivore"]["w_half"])))
+            fitness = 1/(1+math.exp(self.const["Herbivore"]["phi_age"]*(row["age"]-self.const["Herbivore"]["a_half"]))) * 1/(1+math.exp(-self.const["Herbivore"]["phi_weight"]*(row["weight"]-self.const["Herbivore"]["w_half"])))
         return fitness
 
     def aging(self):
         self.age += 1
 
     def feeding(self):
-        self.weight += const["Herbivore"]["beta"]*const["Landskap"]["Lowland"]
+        self.weight += self.const["Herbivore"]["beta"]*self.const["Landskap"]["Lowland"]
+
 
     def loss_of_weight(self):
-        self.weight -= const["Herbivore"]["eta"]*self.weight
+        self.weight -= self.const["Herbivore"]["eta"]*self.weight
 
     def death(self):
-        probility_of_death = self.weight*(1-self.fitness)
-        print(probility_of_death)
+        probility_of_death = self.const["Herbivore"]["omega"]*(1-self.fitness)
         if self.weight <= 0:
-            return print("død")
+            return print(f'status:"død", vekt ved død {self.weight}')
         elif random.random() <= probility_of_death:
-            print(random.random(), probility_of_death)
-            return print("død")
+            print((f'Status:"død", Random(1-0): {random.random()}, Probility of death: {probility_of_death}'))
+            return
         else:
-            return print("levende")
+            print((f'Status:"levende", Random(1-0): {random.random()}, Probility of death: {probility_of_death}'))
+            return
 
 def main(file):
     animals = []
 
 #Start population
-    for row in file[0]["pop"]:
-        animals.append(Animal(row,file[0]["loc"]))
+    for item in file:
+        for row in item["pop"]:
+            animals.append(Animal(row,item["loc"]))
 
 #Annual cycle
-    for year in range(1, 2):
-        print(f'År: {year} Antall dyr: {len(animals)} Dyr[0]alder: {animals[0].age} Dyr[0]vekt: {animals[0].weight} ')
+    for year in range(1, 11):
+        print(f'År: {year} Antall dyr: {len(animals)} Dyr[0]alder: {animals[0].age} Dyr[0]vekt: {animals[0].weight}, Dyr[0]fitness: {animals[0].fitness} ')
         for animal in animals:
+
             #animal.procreation()
             animal.feeding()
+            #animal.calc_fitness(animal)
             #animal.migration()
             animal.aging()
             animal.loss_of_weight()
@@ -96,7 +104,7 @@ if __name__ == "__main__":
                   'pop': [{'species': 'Herbivore',
                            'age': 5,
                            'weight': 20}
-                          for _ in range(150)]}]
+                          for _ in range(1)]}]
 
     animals = main(file)
 
