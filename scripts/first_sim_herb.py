3a8cec3edc641ffad684e1ff7b6a8eb222e577fd
Mathias Mollatt <mathias.mollatt@nmbu.no>
2023-06-06 14:25:32 +0200

    first sim

--- scripts/first_sim_herb.py
+++ scripts/first_sim_herb.py
@@ -11,14 +11,14 @@ ini_herbs = [{'loc': (2, 2),
               'pop': [{'species': 'Herbivore',
                        'age': 5,
                        'weight': 20}
-                      for _ in range(10)]}]
+                      for _ in range(20)]}]
 
 lost = Island(geogr)
 lost.add_population(ini_herbs)
 
 pop_animals = {}
 
-N = 10
+N = 100
 for x in range(1, lost.map_height+1):
     for y in range(1, lost.map_width+1):
         pop_animals[(x,y)] = []
@@ -31,10 +31,10 @@ for year in range(1,N+1):
             cell = lost.map[x][y]
             cell.count_animals()
             #teller dyr i cellen
-            pop_animals[(x,y)].append(cell.count_herbivores())
+            pop_animals[(x,y)].append(cell.count_herbivore)
             #newborn in cell
-            newborn_herbivores = cell.get_newborns(cell.herbivores)
-            newborn_carnivores = cell.get_newborns(cell.carnivores)
+            newborn_herbivores = cell.get_newborns(cell.herbivore)
+            newborn_carnivores = cell.get_newborns(cell.carnivore)
             lost.add_population(newborn_carnivores)
             lost.add_population(newborn_herbivores)
             cell.feed_animals()
