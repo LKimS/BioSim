caccb64fd07af5a170a4b717d85671c4aa7de14c
Kim Ly <kim.son.ly@nmbu.no>
2023-06-06 13:43:09 +0200

    script file to test island.py

---
+++ scripts/check_island.py
@@ -0,0 +1,30 @@
+from biosim.island import Island
+map = """\
+WHW
+LHL
+WDL
+HWW"""
+
+island = Island(map)
+pop = [{'loc': (1, 1),
+        'pop': [{'species': 'Herbivore',
+                 'age': 5,
+                 'weight': 20}
+                for _ in range(150)]},
+
+       {'loc': (2, 2),
+        'pop': [{'species': 'Carnivore',
+                 'age': 5,
+                 'weight': 20}
+                for _ in range(150)]},
+
+       ]
+
+island.add_population(pop)
+
+# bitmap = A.bitmap
+
+print(repr(island.map))
+print(island.map[1][1])
+
+
