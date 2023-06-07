b215d4cc8ccf2f2817f69d10d2d65fddc80585da
Kim Ly <kim.son.ly@nmbu.no>
2023-06-07 09:15:32 +0200

    Added some comments

--- scripts/first_sim_herb.py
+++ scripts/first_sim_herb.py
@@ -2,6 +2,7 @@ from biosim.island import Island
 
 import matplotlib.pyplot as plt
 
+# Map of the island
 geogr = """\
            WWW
            WLW
@@ -11,14 +12,14 @@ ini_herbs = [{'loc': (2, 2),
               'pop': [{'species': 'Herbivore',
                        'age': 5,
                        'weight': 20}
-                      for _ in range(25)]}]
+                      for _ in range(2)]}]
 
 lost = Island(geogr)
 lost.add_population(ini_herbs)
 
 pop_animals = {}
 
-N = 100
+N = 40
 for x in range(1, lost.map_height+1):
     for y in range(1, lost.map_width+1):
         pop_animals[(x,y)] = []
