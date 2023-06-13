84bac6a3c1c4324c49f8a17bce9b23a81db38a21
Kim Ly <kim.son.ly@nmbu.no>
2023-06-13 14:23:48 +0200

    start: graphics.py

---
+++ src/biosim/graphics.py
@@ -0,0 +1,14 @@
+"""
+Class to make graphics for the island
+"""
+import matplotlib.pyplot as plt
+
+
+
+class Graphics:
+    """
+    Class to make graphics for the island
+    """
+    ¨
+    def __init__(self):
+        self.fig = plt.figure()
