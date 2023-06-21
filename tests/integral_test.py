dbebcc53eb65250d3e6f4f76f8c36015b525fa0f
Kim Ly <kim.son.ly@nmbu.no>
2023-06-21 10:47:51 +0200

    minor test change

--- tests/integral_test.py
+++ tests/integral_test.py
@@ -1,7 +1,8 @@
 from biosim.simulation import BioSim
+import pytest
 
-
-def test_migration_integration():
+@pytest.mark.parametrize("species", ["Herbivore", "Carnivore"])
+def test_migration_integration(species):
     """Test migration integration test.
     This forces the animals to move each year.
     The test is checking if the animals are following the chekkerboard pattern."""
@@ -31,10 +32,10 @@ def test_migration_integration():
     sim = BioSim(island_map=geography, seed=23456,
                  cmax_animals={"Herbivore": 1, 'Carnivore': 1},
                  ymax_animals=100, vis_years=0)
-    sim.set_animal_parameters('Herbivore', {'mu': 100000, 'eta': 0})
+    sim.set_animal_parameters(species, {'mu': 100000, 'eta': 0})  # Ensures that the animals move
 
     ini_pop = [{'loc': (10, 10),
-                'pop': [{'species': 'Herbivore',
+                'pop': [{'species': species,
                          'age': 5,
                          'weight': 20}
                         for _ in range(100)]}]
@@ -48,6 +49,7 @@ def test_migration_integration():
             x = loc[0]
             y = loc[1]
 
+            # Check if the animals are following the checkerboard pattern
             if year % 2 == 0:
                 if x % 2 == 0:
                     if y % 2 != 0:
