3e7b11bea4a84b3e068dcadebea39de077250559
Kim Ly <kim.son.ly@nmbu.no>
2023-06-21 02:04:16 +0200

    Deleted files

--- tests/integral_test.py
+++ tests/integral_test.py
@@ -3,50 +3,50 @@ import pytest
 import matplotlib.pyplot as plt
 
 
-def migration_integration_test():
-    """Test migration integration test."""
-    geography = """\
-                    WWWWWWWWWWWWWWWWWWW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHLHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WHHHHHHHHHHHHHHHHHW
-                    WWWWWWWWWWWWWWWWWWW
-                    """
 
-    sim = BioSim(island_map=geography, seed=23456, cmax_animals={"Herbivore": 1})
-    sim.set_animal_parameters('Herbivore', {'mu': 10000000, 'eta': 0})
+"""Test migration integration test."""
+geography = """\
+                WWWWWWWWWWWWWWWWWWW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHLHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WHHHHHHHHHHHHHHHHHW
+                WWWWWWWWWWWWWWWWWWW
+                """
 
-    ini_pop = [{'loc': (10, 10),
-                'pop': [{'species': 'Herbivore',
-                         'age': 5,
-                         'weight': 20}
-                        for _ in range(100)]}]
+sim = BioSim(island_map=geography, seed=23456, cmax_animals={"Herbivore": 1})
+sim.set_animal_parameters('Herbivore', {'mu': 1, 'eta': 0})
 
-    sim.add_population(population=ini_pop)
+ini_pop = [{'loc': (10, 10),
+            'pop': [{'species': 'Herbivore',
+                     'age': 5,
+                     'weight': 20}
+                    for _ in range(100)]}]
 
-    for _ in range(1):
-        sim.simulate(num_years=1)
+sim.add_population(population=ini_pop)
 
-
-    #sim.simulate(num_years=9)
-
-    sim.island.plot_map()
-    #plt.show()
+for _ in range(1):
+    sim.simulate(num_years=3)
 
 
-migration_integration_test()
+#sim.simulate(num_years=9)
+
+sim.island.plot_map()
+#plt.show()
+
+
+
