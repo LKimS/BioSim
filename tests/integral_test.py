d9fc7ebafd24b68bc42d273e4005ee46b76a3a36
Kim Ly <kim.son.ly@nmbu.no>
2023-06-21 03:07:26 +0200

    Deleted files

--- tests/integral_test.py
+++ tests/integral_test.py
@@ -1,52 +1,64 @@
 from biosim.simulation import BioSim
-import pytest
-import matplotlib.pyplot as plt
 
 
+def test_migration_integration():
+    """Test migration integration test.
+    This forces the animals to move each year.
+    The test is checking if the animals are following the chekkerboard pattern."""
+    geography = """\
+                    WWWWWWWWWWWWWWWWWWW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHLHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WHHHHHHHHHHHHHHHHHW
+                    WWWWWWWWWWWWWWWWWWW
+                    """
 
-"""Test migration integration test."""
-geography = """\
-                WWWWWWWWWWWWWWWWWWW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHLHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WHHHHHHHHHHHHHHHHHW
-                WWWWWWWWWWWWWWWWWWW
-                """
+    sim = BioSim(island_map=geography, seed=23456,
+                 cmax_animals={"Herbivore": 1, 'Carnivore': 1},
+                 ymax_animals=100, vis_years=0)
+    sim.set_animal_parameters('Herbivore', {'mu': 100000, 'eta': 0})
 
-sim = BioSim(island_map=geography, seed=23456, cmax_animals={"Herbivore": 1})
-sim.set_animal_parameters('Herbivore', {'mu': 1, 'eta': 0})
+    ini_pop = [{'loc': (10, 10),
+                'pop': [{'species': 'Herbivore',
+                         'age': 5,
+                         'weight': 20}
+                        for _ in range(100)]}]
 
-ini_pop = [{'loc': (10, 10),
-            'pop': [{'species': 'Herbivore',
-                     'age': 5,
-                     'weight': 20}
-                    for _ in range(100)]}]
+    sim.add_population(population=ini_pop)
+    map = sim.island.habital_map
 
-sim.add_population(population=ini_pop)
+    for year in range(1, 8):
+        sim.simulate(num_years=1)
+        for loc, cell in map.items():
+            x = loc[0]
+            y = loc[1]
 
-for _ in range(1):
-    sim.simulate(num_years=3)
-
-
-#sim.simulate(num_years=9)
-
-sim.island.plot_map()
-#plt.show()
-
-
-
+            if year % 2 == 0:
+                if x % 2 == 0:
+                    if y % 2 != 0:
+                        assert cell.animals == []
+                elif x % 2 != 0:
+                    if y % 2 == 0:
+                        assert cell.animals == []
+            elif year % 2 != 0:
+                if x % 2 == 0:
+                    if y % 2 == 0:
+                        assert cell.animals == []
+                elif x % 2 != 0:
+                    if y % 2 != 0:
+                        assert cell.animals == []
