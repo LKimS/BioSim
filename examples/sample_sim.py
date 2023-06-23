2055c2e1bac627bd06d8e9bee2f80f9d9c7705b1
Mathias Mollatt <mathias.mollatt@nmbu.no>
2023-06-23 18:51:02 +0200

    examples

---
+++ examples/sample_sim.py
@@ -0,0 +1,65 @@
+"""
+Full island simulation with herbivores and carnivores
+including movie creation.
+"""
+
+__author__ = 'Hans Ekkehard Plesser, NMBU'
+
+import textwrap
+
+from biosim.simulation import BioSim
+
+if __name__ == '__main__':
+
+    geogr = """\
+               WWWWWWWWWWWWWWWWWWWWW
+               WHHHHHLLLLWWLLLLLLLWW
+               WHHHHHLLLLWWLLLLLLLWW
+               WHHHHHLLLLWWLLLLLLLWW
+               WWHHLLLLLLLWWLLLLLLLW
+               WWHHLLLLLLLWWLLLLLLLW
+               WWWWWWWWHWWWWLLLLLLLW
+               WHHHHHLLLLWWLLLLLLLWW
+               WHHHHHHHHHWWLLLLLLWWW
+               WHHHHHDDDDDLLLLLLLWWW
+               WHHHHHDDDDDLLLLLLLWWW
+               WHHHHHDDDDDLLLLLLLWWW
+               WHHHHHDDDDDWWLLLLLWWW
+               WHHHHDDDDDDLLLLWWWWWW
+               WWHHHHDDDDDDLWWWWWWWW
+               WWHHHHDDDDDLLLWWWWWWW
+               WHHHHHDDDDDLLLLLLLWWW
+               WHHHHDDDDDDLLLLWWWWWW
+               WWHHHHDDDDDLLLWWWWWWW
+               WWWHHHHLLLLLLLWWWWWWW
+               WWWHHHHHHWWWWWWWWWWWW
+               WWWWWWWWWWWWWWWWWWWWW"""
+    geogr = textwrap.dedent(geogr)
+
+    ini_herbs = [{'loc': (2, 7),
+                  'pop': [{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(200)]}]
+    ini_carns = [{'loc': (2, 7),
+                  'pop': [{'species': 'Carnivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(50)]}]
+
+    sim = BioSim(geogr, ini_herbs, seed=1,
+                 hist_specs={'fitness': {'max': 1.0, 'delta': 0.05},
+                             'age': {'max': 60.0, 'delta': 2},
+                             'weight': {'max': 60, 'delta': 2}},
+                 cmax_animals={'Herbivore': 100, 'Carnivore': 100},
+                 img_dir='results', ymax_animals= 20000,
+                 img_base='sample')
+    sim.simulate(100)
+    sim.add_population(ini_carns)
+    sim.simulate(300)
+    sim.set_animal_parameters('Herbivore', {'zeta': 3.2, 'xi': 1.8})
+    sim.set_animal_parameters('Carnivore', {'a_half': 70, 'phi_age': 0.5,
+                                            'omega': 0.3, 'F': 65,
+                                            'DeltaPhiMax': 9.})
+    sim.simulate(100)
+    sim.make_movie()
